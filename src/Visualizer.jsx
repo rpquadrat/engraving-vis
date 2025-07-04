9545f3d2d12c7a73d8da27155933380907c2bfda
Roland Pavic <roland@rpquadrat.de>
2025-07-04 23:30:20 +0200

    Before Grid changes

---
+++ src/Visualizer.jsx
@@ -0,0 +1,163 @@
+import {useEffect, useRef, useState} from "react";
+import { subscribe, unsubscribe } from "./events";
+import VizCanvas from "./VizCanvas";
+import TextControls from './TextControls';
+import Faqs from "./Faqs";
+import Ideas from './Ideas';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import contentData from './content';
+
+const alignLeft = {style: "left", value: "29"}; // left
+const alignCenter = {style: "center", value: "45.5"}; // center
+const alignRight = {style: "right", value: "61.5"}; // right
+
+function Visualizer(props) {
+
+  const [appData, setAppData] = useState({
+    firstLine: "",
+    secondLine: "",
+    thirdLine: "",
+    align: {
+      style: "center",
+      value: "45.5",
+    },
+  });
+  const [appConfig, setAppConfig] = useState({
+    firstLinePlaceholder: props.root.dataset.firstLinePlaceholder || "",
+    secondLinePlaceholder: props.root.dataset.secondLinePlaceholder || "",
+    thirdLinePlaceholder: props.root.dataset.thirdLinePlaceholder || "",
+    firstLineDisabled: props.root.dataset.firstLineDisabled || false,
+    secondLineDisabled: props.root.dataset.secondLineDisabled || false,
+    thirdLineDisabled: props.root.dataset.thirdLineDisabled || false,
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [opacity, setOpacity] = useState(0);
+  const [appLang, setAppLang] = useState("de");
+
+  useEffect(() => {
+    subscribe("showVisualizer", openOverlay);
+
+    const isoAttr = document.querySelector("[id^='attr_engraving_data_']");
+    if (isoAttr && isoAttr.value !== "") {
+      const isoAttrData = JSON.parse(isoAttr.value);
+      if (appConfig.firstLineDisabled) {
+        setAppData({...isoAttrData, firstLine: appConfig.firstLinePlaceholder});
+      } else {
+        setAppData(isoAttrData);
+      }
+    } else if (appConfig.firstLineDisabled) {
+      setAppData({...appData, firstLine: appConfig.firstLinePlaceholder});
+    }
+
+    return () => {
+      unsubscribe("showVisualizer", closeOverlay);
+    }
+  }, []);
+
+  useEffect(() => {
+    const docLang = document.documentElement.lang;
+    setAppLang(docLang);
+  }, []);
+
+  /*useEffect(() => {
+    if (appConfig.firstLineDisabled) setAppData({...appData, firstLine: appConfig.firstLinePlaceholder});
+  }, []);*/
+
+  function inputHandler(event) {
+    setAppData((prevAppData) => {
+      return {
+        ...prevAppData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function alignHandler(event) {
+    setAppData((prevAppData) => {
+      return {
+        ...prevAppData,
+        align: alignSelector(event.target.value),
+      };
+    });
+  }
+
+  function alignSelector(val) {
+    switch (val) {
+      case "left":
+        return alignLeft;
+      case "center":
+        return alignCenter;
+      case "right":
+        return alignRight;
+      default:
+        return alignCenter;
+    }
+  }
+
+  function saveData () {
+    // stringify appData and close the overlay
+    const isoAttr = document.querySelector("[id^='attr_engraving_data_']");
+    if (isoAttr) isoAttr.value = JSON.stringify(appData);
+    closeOverlay();
+  }
+
+  function openOverlay() {
+    setIsOpen(true);
+    document.body.style.overflow = 'hidden';
+    setTimeout(() => {
+      setOpacity(1);
+    }, 100);
+  }
+  function closeOverlay() {
+    setOpacity(0);
+    setTimeout(() => {
+      setIsOpen(false);
+      document.body.style.overflow = 'auto';
+    }, 400);
+  }
+
+  return (
+    isOpen && <div className="engraver" style={{opacity: opacity}}>
+      <div className="engraver-overlay-bg"></div>
+        <Container className="engraver-overlay -color-inverted">
+          <Row className="engraver-title">
+            <Col xs={12}>
+              <h3>{appLang === "de" ? contentData.de.title : contentData.en.title}</h3>
+              <button type="button" className="engraver-close-button -tertiary" aria-hidden="false" aria-label="Close" onClick={closeOverlay}>X</button>
+            </Col>
+          </Row>
+          <Row className="engraver-content">
+          <Col className="engraver-image" xs={12} md={7}>
+            <VizCanvas firstLine={appData.firstLine} secondLine={appData.secondLine} thirdLine={appData.thirdLine} align={appData.align} />
+          </Col>
+          <Col className="engraver-tools" xs={12} md={5}>
+            <TextControls
+              introText={appLang === "de" ? contentData.de.introText : contentData.en.introText}
+              contentData={appLang === "de" ? contentData.de : contentData.en}
+              appData={appData}
+              appConfig={appConfig}
+              inputHandler={inputHandler}
+              alignHandler={alignHandler}
+              saveData={saveData} />
+          </Col>
+          </Row>
+          <Row className="engraver-ideas">
+            <Col xs={12}>
+              <h3>{appLang === "de" ? contentData.de.ideasHeader : contentData.en.ideasHeader}</h3>
+              <Ideas items={appLang === "de" ? contentData.de.ideasData : contentData.en.ideasData} />
+            </Col>
+          </Row>
+          <Row className="engraver-faqs">
+            <Col xs={12}>
+              <h3>{appLang === "de" ? contentData.de.faqsHeader : contentData.en.faqsHeader}</h3>
+              <Faqs items={appLang === "de" ? contentData.de.faqsData : contentData.en.faqsData} />
+            </Col>
+          </Row>
+        </Container>
+
+    </div>
+  );
+}
+export default Visualizer;
---
+++ src/content.js
@@ -0,0 +1,185 @@
+const contentData = {
+  de: {
+    title: 'G44 Gravur Konfigurator',
+    introText: 'Sie können eine dreizeilige Gravur mit jeweils 16 Zeichen erstellen*',
+    saveButton: 'Gravur speichern',
+    hintHeader: '*Hinweis:',
+    hintText: 'Bitte positionieren Sie die Elemente nicht mit zusätzlichen Leerzeichen.',
+    ideasHeader: 'Gravur Ideen',
+    ideasData: [
+      {
+        sectionName: 'Berühmte Gravuren',
+        sectionData: [
+          {
+            firstLine: 'Drive Slowly!',
+            secondLine: 'Drive Carefully!',
+            thirdLine: '(Paul Newmann)',
+          },
+          {
+            firstLine: 'To Haic',
+            secondLine: 'Le Mans',
+            thirdLine: '1970',
+          },
+          {
+            firstLine: 'm.',
+            secondLine: 'Brando',
+            thirdLine: '(Marlon Brando)',
+          }
+        ],
+      },
+      {
+        sectionName: 'Meilensteine im Leben',
+        sectionData: [
+          {
+            firstLine: 'Abitur 2024',
+            secondLine: 'Leon',
+            thirdLine: 'Guinand',
+          },
+          {
+            firstLine: 'Master 2024',
+            secondLine: 'Maximilian',
+            thirdLine: 'Cum Laude',
+          },
+          {
+            firstLine: 'Happy Birthday',
+            secondLine: 'Guinand!',
+            thirdLine: '21.04.1865',
+          }],
+      },
+      {
+        sectionName: 'Liebe / Hochzeit / Geburt',
+        sectionData: [
+          {
+            firstLine: '24.04.2024',
+            secondLine: 'in Liebe',
+            thirdLine: 'Emily',
+          },
+          {
+            firstLine: 'Ja!',
+            secondLine: 'Sina & Alex',
+            thirdLine: '06.06.2024',
+          },
+          {
+            firstLine: 'Ein Mädchen!',
+            secondLine: 'Luise',
+            thirdLine: '08.05.2024',
+          }],
+      },
+    ],
+    faqsHeader: 'FAQs',
+    faqsData: [
+      {
+        question: 'Wie viele Zeichen kann ich gravieren lassen?',
+        answer: 'Für die individuelle Gravur der Deepwave stehen Ihnen drei Mal 16 Zeichen zur Verfügung. Aufgrund der unterschiedlichen Breiten der Zeichen, kann es sein dass Ihr Text optisch auch weniger Zeichen erlaubt. Sie können den Textblock zentriert, linksbündig oder rechtsbündig ausgeben. Probieren Sie aus, was am passendsten ist.',
+      },
+      {
+        question: 'Welche Zeichen kann ich gravieren?',
+        answer: ' Unser Gravur-Konfigurator erlaubt 16 Zeichen pro Zeile, die Gravur wird in der Schriftart Source Sans ausgeführt. Alle regulären Zeichen und Sonderzeichen sind erlaubt. Die Schrifthöhe beträgt ca. 1 mm.',
+      },
+      {
+        question: 'Wie lange dauert eine Gravur?',
+        answer: 'Zur normalen Fertigungszeit müssen Sie noch ungefähr 14 Werktage für die Gravur einrechnen.',
+      },
+      {
+        question: 'Wie funktioniert der Gravurprozess?',
+        answer: 'Schreiben Sie Ihre Nachricht in die vorgegebenen Zeilen – Ihr Text erscheint auf der Simulation daneben. Ist alles in Ordnung? Dann klicken Sie auf „Gravur speichern“ und legen Sie die Uhr in den Warenkorb.',
+      },
+      {
+        question: 'Kann ich eine gravierte Uhr umtauschen oder zurückgeben?',
+        answer: 'Personalisierte Uhren können leider weder zurückgegeben, noch umgetauscht werden.',
+      }
+    ]
+  },
+  en: {
+    title: 'G44 Engraving Configurator',
+    introText: 'Please create a three-line engraving with 16 characters each*',
+    saveButton: 'Save Engraving',
+    hintHeader: 'Hint:',
+    hintText: 'Please do not place elements with extra spaces.',
+    ideasHeader: 'Engraving Ideas',
+    ideasData: [
+      {
+        sectionName: 'Famous Engravings',
+        sectionData: [
+          {
+            firstLine: 'Drive Slowly!',
+            secondLine: 'Drive Carefully!',
+            thirdLine: '(Paul Newmann)',
+          },
+          {
+            firstLine: 'To Haic',
+            secondLine: 'Le Mans',
+            thirdLine: '1970',
+          },
+          {
+            firstLine: 'm.',
+            secondLine: 'Brando',
+            thirdLine: '(Marlon Brando)',
+          }],
+      },
+      {
+        sectionName: 'Milestones in Life',
+        sectionData: [
+          {
+            firstLine: 'Abitur 2024',
+            secondLine: 'Leon',
+            thirdLine: 'Guinand',
+          },
+          {
+            firstLine: 'Master 2024',
+            secondLine: 'Maximilian',
+            thirdLine: 'Cum Laude',
+          },
+          {
+            firstLine: 'Happy Birthday',
+            secondLine: 'Guinand!',
+            thirdLine: '21.04.1865',
+          }],
+      },
+      {
+        sectionName: 'Love / Birth / Wedding',
+        sectionData: [
+          {
+            firstLine: '24.04.2024',
+            secondLine: 'in Love',
+            thirdLine: 'Emily',
+          },
+          {
+            firstLine: 'Yes!',
+            secondLine: 'Sina & Alex',
+            thirdLine: '06.06.2024',
+          },
+          {
+            firstLine: 'A Girl!',
+            secondLine: 'Luise',
+            thirdLine: '08.05.2024',
+          }],
+      },
+    ],
+    faqsHeader: 'FAQs',
+    faqsData: [
+      {
+        question: 'How many characters can I have engraved?',
+        answer: 'You have three 16 characters available for individual engraving of the Deepwave. Due to the different widths of the characters, your text may visually allow for fewer characters. You can output the text block centered, left-aligned, or right-aligned. Try what is most suitable.',
+      },
+      {
+        question: 'Which characters can I engrave?',
+        answer: 'Our engraving configurator allows 16 characters per line, the engraving is carried out in the Source Sans font. All regular characters and special characters are allowed. The font height is approx. 1 mm.',
+      },
+      {
+        question: 'How long does an engraving take?',
+        answer: 'In addition to the normal production time, you must allow approximately 14 working days for the engraving.',
+      },
+      {
+        question: 'How does the engraving process work?',
+        answer: 'Write your message on the lines provided - your text will appear on the simulation next to it. Is everything ok? Then click on “Save engraving” and put the watch in the shopping cart.',
+      },
+      {
+        question: 'Kann ich eine gravierte Uhr umtauschen oder zurückgeben?',
+        answer: 'Unfortunately, personalized watches cannot be returned or exchanged.',
+      }
+    ]
+  }
+};
+
+export default contentData;
